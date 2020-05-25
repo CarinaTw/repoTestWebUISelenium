3c435cdeefe27807d4609306fe7f0101d25bd629
carina <carinka004@gmail.com>
2020-05-25 03:14:08 +0300

    Opencart add exeptions add check login and logout pages add check catalog page

--- test_opencart_catalog.py
+++ test_opencart_catalog.py
@@ -8,6 +8,8 @@ import pytest
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from page import Page
+from time import sleep
+import re
 
 
 class CatalogPage(Page):
@@ -17,41 +19,23 @@ class CatalogPage(Page):
         self.driver = driver
 
 
-def test_catalog_page_title(driver_factory):
+@pytest.mark.parametrize("menu_item", [('Desktops', 'Laptops & Notebooks', 'Components', 'MP3 Players')])
+def test_catalog_page_nav_bar_dropdown_item_show_all(driver_factory, menu_item):
     cat_page = CatalogPage(driver_factory)
     cat_page.driver.get(cat_page.url)
-    assert cat_page.driver.title == 'Desktops'
-
-
-@pytest.mark.parametrize("menu_item", ['Desktops', 'Laptops & Notebooks', 'Components', 'MP3 Players'])
-def test_catalog_page_nav_bar_dropdown_items(driver_factory, menu_item):
-    links_list = ['Desktops', 'Laptops & Notebooks', 'Components', 'MP3 Players']
-    cat_page = CatalogPage(driver_factory)
-    cat_page.driver.get(cat_page.url)
-    for i in range(len(links_list)):
+    wait = WebDriverWait(driver_factory, 3)
+    for i in range(4):
         menu_item = cat_page.driver.find_elements(By.XPATH, "//ul[@class='nav navbar-nav']/li[@class='dropdown']")[i]
         ActionChains(cat_page.driver).move_to_element(menu_item).pause(2).perform()
-        cat_page.driver.find_elements(By.XPATH, "//div[@class='dropdown-menu']/a[@class='see-all']")[i].click()
+        btn = cat_page.driver.find_elements(By.XPATH, "//div[@class='dropdown-menu']/a[@class='see-all']")[i]
+        btn.click()
         link_txt = cat_page.driver.find_elements(By.XPATH, "//ul[@class='nav navbar-nav']/li[@class='dropdown']")[i].text
         page_txt = cat_page.driver.find_element(By.XPATH, "//div[@id='content']/h2").text
+        assert wait.until(EC.url_matches(r'http:\/\/localhost\/index\.php\?route=product\/category\&path\=\d*'))
         assert link_txt == page_txt
 
 
-def test_catalog_page_nav_bar_item_pc(driver_factory):
-    cat_page = CatalogPage(driver_factory)
-    cat_page.driver.get(cat_page.url)
-    elem = cat_page.driver.find_element(By.XPATH,
-                                        "//ul[@class='nav navbar-nav']/li[@class='dropdown']/"
-                                        "a[@class='dropdown-toggle']")
-    elem.click()
-    ActionChains(cat_page.driver).move_to_element(elem).pause(2).perform()
-    cat_page.driver.find_element(By.XPATH, "//ul[@class='list-unstyled']/li[1]").click()
-    cat_page.driver.find_element_by_partial_link_text('PC')
-    text = cat_page.driver.find_element(By.XPATH, "//div[@id='content']//p")
-    assert text.text == 'There are no products to list in this category.'
-
-
-def test_catalog_page_button_wish_list(driver_factory):
+def test_catalog_page_button_wish_list_check(driver_factory):
     cat_page = CatalogPage(driver_factory)
     cat_page.driver.get(cat_page.url)
     product_grid = cat_page.driver.find_element(By.XPATH, "//div[@id='content']/div[@class='row'][4]")
@@ -59,11 +43,26 @@ def test_catalog_page_button_wish_list(driver_factory):
         ".product-layout.product-grid.col-lg-4.col-md-4.col-sm-6.col-xs-12")[1]
     button_heart = grid_item.find_element(By.XPATH, "//button[@data-original-title='Add to Wish List']")
     button_heart.click()
-    alert_item = WebDriverWait(cat_page.driver, 10).until(EC.visibility_of_element_located((By.XPATH,
+    wait = WebDriverWait(driver_factory, 3)
+    alert_item = wait.until(EC.visibility_of_element_located((By.XPATH,
                                                           "//div[@class='alert alert-success alert-dismissible']")))
     ActionChains(cat_page.driver).move_to_element(alert_item).perform()
-    assert alert_item.find_element_by_partial_link_text('login').text == 'login'
-    assert alert_item.find_element_by_partial_link_text('create an account').text == 'create an account'
-    assert alert_item.find_element_by_partial_link_text('wish list').text == 'wish list'
+    assert re.search("You must login or create an account to save", alert_item.text)
 
 
+@pytest.mark.xfail
+def test_catalog_page_button_add_to_cart_check(driver_factory):
+    cat_page = CatalogPage(driver_factory)
+    cat_page.driver.get(cat_page.url)
+    wait = WebDriverWait(driver_factory, 3)
+
+    product_grid = cat_page.driver.find_element(By.XPATH, "//div[@id='content']/div[@class='row'][4]")
+    grid_item = product_grid.find_elements_by_css_selector(
+        ".product-layout.product-grid.col-lg-4.col-md-4.col-sm-6.col-xs-12")[1]
+    button_cart = grid_item.find_elements(By.XPATH, "//i[@class='fa fa-shopping-cart']")
+    sleep(3)
+    button_cart[2].click()
+    alert_item = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                          "//div[@class='alert alert-success alert-dismissible']")))
+    ActionChains(cat_page.driver).move_to_element(alert_item).perform()
+    assert re.search("Success: You have added", alert_item.text)
